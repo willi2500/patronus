d071c729e5bdc0fd3cde0b74f893ddf48a374953
andy <arteysoft@gmail.com>
2022-10-12 11:51:04 -0300

    sin cambios

---
+++ clase1/src/main/java/edu/it/factories/Factory.java
@@ -0,0 +1,14 @@
+package edu.it.factories;
+
+import edu.it.repository.GrabadorSuscriptor;
+import edu.it.repository.GrabadorSuscriptorSQL;
+import edu.it.service.LectorSuscriptor;
+
+public class Factory {
+	public static GrabadorSuscriptor obtenerGrabadorSuscriptor() {
+		return new GrabadorSuscriptorSQL();
+	}
+	public static LectorSuscriptor obtenerLectorSuscriptor() {
+		return new edu.it.service.LectorSuscriptorConsola();
+	}
+}
